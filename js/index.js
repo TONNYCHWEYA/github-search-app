dc87d9e3f8f9dc0436d7e2e0204c87d81be37a4f
TONNYCHWEYA <chweyatonny@gmail.com>
2022-07-03 23:27:59 +0300

    Done.

--- js/index.js
+++ js/index.js
@@ -10,9 +10,34 @@ function getUser(inputValue){
   fetch(`https://api.github.com/search/users?q=${inputValue}`)
   .then(res => res.json())
   .then(data => {
+    console.log(data)
     const usersArray = data.items
     usersArray.forEach(user => {
-      console.log(user)
+      createLi(user)
     })
   })
+}
+
+
+const ul = document.getElementById("user-list")
+function createLi(user){
+  
+  const li = document.createElement("li")
+  li.innerHTML =`
+  <p>User_Name: ${user.login}</p>
+  <p>Avatar_url: ${user.avatar_url}</p>
+  <p>Profile link: ${user.html_url}</p>
+  `
+ li.addEventListener('click', (event) => {
+   const githubName =user.login
+   getRepo(githubName)
+ })
+  ul.appendChild(li)
+  
+}
+
+function getRepo(githubName){
+  fetch(`https://api.github.com/users/${githubName}/repos`)
+  .then(res => res.json())
+  .then(data => console.log(data))
 }
